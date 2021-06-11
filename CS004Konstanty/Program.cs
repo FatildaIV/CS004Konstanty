5757239fa370123bd9341a7c2a1b3b9dfb906e2d
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:09:57 +0200

    Add files via upload

---
+++ CS004Konstanty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS004Konstanty
+{
+    class Program
+    {
+        const int ZpozdeniPismeno = 200;
+        const int ZpozdeniTecka = 50;
+        const int ZpozdeniCarka = 150;
+
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.Write("Karel Hofman");
+        }
+    }
+}
